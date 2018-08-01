e75ad08d0cf0dfc1f8c057e07a814bbcd6956ba8
Voss, Jennifer (ELS-PHI) <j.voss@elsevier.com>
2018-07-31 21:53:13 -0400

    Show previously marked items regardless of active state

--- src/UpdateChain.js
+++ src/UpdateChain.js
@@ -4,13 +4,6 @@ import Toggle from './Toggle';
 // import './Update-chain.css';
 
 class UpdateChain extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newName: ''
-    };
-  }
-
   toggleValue = (key, val) => {
     const datesRef = 'users/' + this.props.uid + '/dates/' + this.props.date;
     db.ref(datesRef + '/' + key).set(val);
@@ -21,16 +14,26 @@ class UpdateChain extends Component {
   }
 
   render() {
-    const chains = Object.keys(this.props.chains)
-      .filter(key => !!this.props.chains[key].show)
+    const activeChainKeys = Object.keys(this.props.chains)
+      .filter(key => !!this.props.chains[key].show);
+
+    const activeChains = activeChainKeys
       .map(key => Object.assign({key: key}, this.props.chains[key]));
 
-      return (
+    let previousChains = [];
+    const markedDates = this.props.dates[this.props.date];
+    if (markedDates) {
+      previousChains = Object.keys(markedDates)
+        .filter(key => activeChainKeys.indexOf(key) === -1)
+        .map(key => Object.assign({ key: key }, this.props.chains[key]));
+    }
+
+    return (
       <div className="settings">
         <h1>Did you do the things?</h1>
 
         <ul className="chain-list">
-          {chains.map((item) => {
+          {activeChains.map((item) => {
             return (
               <li key={item.key}>
                 <span>{item.name}</span>
@@ -41,6 +44,19 @@ class UpdateChain extends Component {
               </li>
             );
           })}
+
+          {previousChains.map(item => {
+            return (
+              <li style={{opacity: '0.25'}} key={item.key}>
+                <span>{item.name}</span>
+                <Toggle
+                  disabled
+                  value={!this.getValue(item.key)}
+                />
+              </li>
+            );
+          })}
+
         </ul>
       </div>
     );
